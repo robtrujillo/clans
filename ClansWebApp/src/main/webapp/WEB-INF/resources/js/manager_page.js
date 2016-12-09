af56ba07820c083f69525cb9fde38ad753a6e9e8
jotung <joseph.tung@stonybrook.edu>
2016-12-09 04:02:10 -0500

    test

--- ClansWebApp/src/main/webapp/WEB-INF/resources/js/manager_page.js
+++ ClansWebApp/src/main/webapp/WEB-INF/resources/js/manager_page.js
@@ -8,8 +8,6 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope, $http) {
 
     $scope.userId = parseInt($("#userId").val());
-    $scope.pageId = parseInt($("#pageId").val());
-    
 
     var x = $http({
         method: 'GET',
@@ -19,7 +17,6 @@ app.controller('myCtrl', function ($scope, $http) {
         $scope.posts = response.data;
         x = 0;
         while (x < $scope.posts.length) {
-
             $scope.posts[x]["comments"] = [];
             x++;
         }
@@ -29,54 +26,200 @@ app.controller('myCtrl', function ($scope, $http) {
     }, function errorCallBack(response) {
         alert("error in get posts");
     });
-    $scope.index = 0
-    $scope.getComments = function (postId) {
-        x = postId;
-        index = 0;
-        for (; postId !== $scope.posts[index].postId; index++)
-            ;
+    $scope.index = 0;
 
+    $scope.changeHello = function () {
+        $scope.HELLO = "CHANGED BAM!";
+    };
+
+    $scope.deleteAd = function () {
         var y = $http({
             method: 'GET',
-            url: '/ClansWebApp/getComments',
-            params: {"postId": x}
+            url: '/ClansWebApp/byeAd',
+            params: {"adId": $scope.selectedAd.adId}
         }).then(function (response) {
-            $scope.posts[index]["comments"] = response.data
+            alert("Deleted Ad:");
+            $scope.getAllAds();
         }, function errorCallBack(response) {
-            alert("get comments error");
+            alert("delete Ad error");
+        });
+    };
+
+
+
+
+    $scope.updateSales = function () {
+        var y = $http({
+            method: 'GET',
+            url: '/ClansWebApp/createSales',
+            params: {"adId": $scope.ssAd.adId, "accountId": $scope.ssAcct.accountId,
+                "numUnits": parseInt($scope.NumUnits)}
+        }).then(function (response) {
+            alert("Added new Sales:");
+        }, function errorCallBack(response) {
+            alert("Update sales error");
+        });
+    };
+
+    $scope.updateAd = function () {
+        x = $scope.userId;
+        var y = $http({
+            method: 'GET',
+            url: '/ClansWebApp/createAd',
+            params: {"itemType": $scope.itemType, "itemName": $scope.itemName,
+                "company": $scope.company, "content": $scope.content,
+                "unitPrice": parseFloat($scope.unitPrice), "numAvailable": parseInt($scope.numAvaliable),
+                "userId": x}
+        }).then(function (response) {
+            alert("Added New Ad:");
+        }, function errorCallBack(response) {
+            alert("Update Ad error");
+        });
+    };
+
+    $scope.ads = [];
+    $scope.getAllAds = function () {
+        $http({
+            method: 'GET',
+            url: '/ClansWebApp/getAllAds'
+        }).then(function (response) {
+            $scope.ads = response.data;
+        }, function errorCallBack(response) {
+            alert("error in get allAds");
+        });
+    };
+
+    $scope.ads2 = [];
+    $scope.getAllAds2 = function () {
+        $http({
+            method: 'GET',
+            url: '/ClansWebApp/getAllAds'
+        }).then(function (response) {
+            $scope.ads2 = response.data;
+        }, function errorCallBack(response) {
+            alert("error in get allAds");
+        });
+    };
+
+    $scope.accts = [];
+    $scope.getAllAccts = function () {
+        $http({
+            method: 'GET',
+            url: '/ClansWebApp/getAllAcct'
+        }).then(function (response) {
+            $scope.accts = response.data;
+        }, function errorCallBack(response) {
+            alert("error in get allAds");
+        });
+    };
+
+    $scope.accts2 = [];
+    $scope.getAllAccts2 = function () {
+        $http({
+            method: 'GET',
+            url: '/ClansWebApp/getAllAcct'
+        }).then(function (response) {
+            $scope.accts2 = response.data;
+        }, function errorCallBack(response) {
+            alert("error in get allAds");
+        });
+    };
+
+    $scope.names2 = [];
+    $scope.getNames2 = function () {
+        $http({
+            method: 'GET',
+            url: '/ClansWebApp/getAllUsers',
+            params: {"userId": 0}
+        }).then(function (response) {
+            $scope.names2 = response.data;
+        }, function errorCallBack(response) {
+            alert("error in get posts");
+        });
+    };
+
+
+    $scope.getRTList = function () {
+        $scope.getAllAccts();
+        $scope.getAllAds2();
+    };
+
+    $scope.accountState = -1;
+
+    $scope.addAcctBtn = function () {
+        $scope.accountState = 0;
+        $scope.getAllAccts2();
+        $scope.getNames2();
+    };
+
+    $scope.editAcctBtn = function () {
+        $scope.accountState = 1;
+        $scope.getAllAccts2();
+    };
+    $scope.deleteAcctBtn = function () {
+        $scope.accountState = 2;
+        $scope.getAllAccts2();
+    };
+//
+//    private int accountId; userId
+//            private String creditCardNum;
+    $scope.acct2Select = null;
+    $scope.user2Select = null;
+    
+      $scope.deleteAccount = function () {
+        x = $scope.acct2Select === null ? 0 : $scope.acct2Select.accountId;
+        $http({
+            method: 'GET',
+            url: '/ClansWebApp/deleteAccount',
+            params: {"accountId":x}
+        }).then(function (response) {
+            alert("deleted acount");
+            $scope.getAllAccts2();
+        }, function errorCallBack(response) {
+            alert("error in delete Account");
         });
     };
     
-    $scope.editComment = function(i1, i2){
-        postIndex = i1;
-        commentIndex = i2;
-        comment = $scope.posts[postIndex].comments[commentIndex];
-        var y = $http({
+    
+    $scope.updateAccount = function () {
+        x = $scope.acct2Select === null ? 0 : $scope.acct2Select.accountId;
+        u = $scope.user2Select === null ? 0 : $scope.user2Select.userId;
+        $http({
             method: 'GET',
-            url: '/ClansWebApp/saveComment',
-            params: {"commentId":comment.commentId, "content": comment.content}
+            url: '/ClansWebApp/changeAccount',
+            params: {"userId":u ,"accountId":x, "creditCardNum":$scope.ccNum}
         }).then(function (response) {
-            $scope.posts[index]["comments"] = response.data;
+            alert("updated acount");
         }, function errorCallBack(response) {
-            alert("get comments error\n");
+            alert("error in update Account");
+        });
+    };
+
+    $scope.names = [];
+    $scope.getNames = function () {
+        $http({
+            method: 'GET',
+            url: '/ClansWebApp/getAllUsers',
+            params: {"userId": 0}
+        }).then(function (response) {
+            $scope.names = response.data;
+        }, function errorCallBack(response) {
+            alert("error in get posts");
+        });
+    };
+});
+    
+    $scope.salesreport = []
+    $scope.getSales = function() {
+        $http({
+            method: 'GET',
+            url: '/ClansWebApp/getSales'
+        }).then(function(respose) {
+            $scope.salesreport = response.data;
+        }, function errorCallBack(response) {
+            alert("error in get sales");
         });
     }
-    $scope.sendPost = function() {
-        var data = $.param({
-            json: JSON.stringify({
-                commentId: comment.commentId
-            })
-        });
-        $http.post("/echo/json/", data).success(function(data, status) {
-            $scope.hello = data;
-        })
-    } 
-
-
-
-
-});
-
 
 
 
